f828f8ccbd5fa04a51ae9305fc98519671078ca2
Leticia <134729304+LeticiaAraujoTDS@users.noreply.github.com>
2023-11-29 16:27:10 -0300

    Update pokedex-index.php

--- pokedex-index.php
+++ pokedex-index.php
@@ -1,71 +1,63 @@
 <html>
 
-    <head>
-
-        <title>pokedex</title>
-
-        <style>
-
-            #pesquisa {
-
-                background: #c92626;
-                font-family: Verdana, Geneva, Tahoma, sans-serif;
-                padding: 20px;
-                text-align: center;
-
-            }
-
-            .pokemon{
-
-                width: 20%;
-                border: solid 2px #555;
-                padding: 15px;
-                margin: 10px 10px 10px 10px;
-                /* */
-                float: left;
-
-            }
-
-            .pokemon img {
-
-                max-width: 100%;
-
-            }
-
-        </style>
+<head>
+    <title>Pokedex</title>
 
 
-    </head>
+    <style>
+
+        #pesquisa {
+            
+            background: #c92626;
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+            padding: 20px;
+            text-align: center;
+        }
+
+        .pokemon {
+            width: 20%;
+            border: solid 2px #555;
+            padding: 15px;/* margem interna  */
+            margin: 10px 10px 10px 10px;
+            float: left;
+            
+        }
+
+        .pokemon img {
+            max-width: 100%;
+        }
+
+    </style>
+
+
+</head>
 
 <body>
 
     <div id="pesquisa" >
         <form>
-            <input type="text" placeholder="Digite um Pokémon">
-            <input type="submit" value="buscar">
+            <input type="text" placeholder="Digite um Pokémon" >
+            <input type="submit" value="buscar" >
         </form>
     </div>
 
     <div id="pokemons">
 
-    <?php for($i = 0; $i < 20; $i++): ?>
-
+        <?php for($i = 0; $i < 20; $i++): ?>
         <div class="pokemon">
 
-            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/015.png" alt=" Beedrill" width="200px">
-            #src é o link do serch no google e alt é o nome da imagemque vai aparacer
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png" alt="Jigglypuff" width="200px">
 
             <h1>Jigglypuff</h1>
-            <p>peso: 29.5</p>
-            <p>altura: 1.0</p>
-
+            <p>peso: 0.8</p>
+            <p>altura 0.8</p>
         </div>
-    <?php endfor; ?>
+        <?php endfor; ?>
+
 
     </div>
 
 
-
+    
 </body>
-
 </html>
